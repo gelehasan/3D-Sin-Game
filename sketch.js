b7054d09cc25dabad9923e52c77ffa3452fb784a
Gele <gellehussen@gmail.com>
2023-08-01 21:34:20 +0200

    boxes are now drawn on the canvas

--- sketch.js
+++ sketch.js
@@ -19,6 +19,23 @@ function draw() {
 function drawBoxes() {
     
     
-}
+        for (let i = -400; i < 400; i=i+ 50) {
+          for (let j = -400; j < 400; j= j+ 50) {
+            let distance = dist(0, 0, i, j) + (frameCount * 1);
+            const minHeight = 100;
+            const maxHeight = 300;
+            let length = map(sin(distance), -1, 1, minHeight, maxHeight);
+      
+            
+            push();
+            normalMaterial(); 
+            stroke(0);
+            strokeWeight(2);
+            translate(i, 0, j);
+            box(50, length, 50);
+            pop();
+          }
+        }
+      }
   
 
